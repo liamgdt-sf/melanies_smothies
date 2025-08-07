cbcc6837bf368579eb92bff73ace446b483b9477
liamgdt-sf <liamgrantdetorres@kubrickgroup.com>
2025-08-07 12:04:21 +0000

    Adding search on column and using for request

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,10 @@ st.write('The name on your Smoothie will be :' , name_on_order)
 
 
 my_dataframe = session.table('smoothies.public.fruit_options'
-                            ).select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe , use_container_width=True)
+                            ).select(col('FRUIT_NAME') , col('search_on'))
 
+st.dataframe(data=my_dataframe , use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect('Chose up to 5 ingredients' ,
                                 my_dataframe ,
